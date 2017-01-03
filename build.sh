b1c1a11922b5f64dc8659971d092ccca346165d7
Jaan Sepp, Anton Keks <jaan@codeborne.com, anton@codeborne.com>
2017-01-03 16:04:01 +0200

    fixing build

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ TARGET=/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
 
 rm -fr dist
 play build-module
-zip --delete dist/*.zip "modules/secure"
 
 if [ -e $TARGET ]; then
     echo "Not publishing, $MODULE-$VERSION already exists"
